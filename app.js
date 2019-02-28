6f3821f6a683767d46da7c42910b1d63e9c1388b
yeni <rnrdj369@gmail.com>
2019-02-28 11:51:39 +0900

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let express = require('express');
-
+let path = require('path');
 let app = express();
 
-app.get('/',function(req,res){
-  res.send('hello world yeni! test22');
-});
+// app.get('/',function(req,res){
+//   res.send('hello world yeni! test22');
+// });
 
-app.listen(5000,function(){
+app.use(express.static(path.join(__dirname + '/public')));
+console.log(__dirname);
+app.listen(8000,function(){
   console.log('Server On!!!vv');
 })
